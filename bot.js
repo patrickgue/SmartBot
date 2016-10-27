3c783f9a4009099a168e8f6ce0ec5f29c3106c32
patrickgue <me@patrickguenthard.ch>
2016-10-27 09:10:34 +0200

    added dictionary example (can't test, fails behind proxy)

--- bot.js
+++ bot.js
@@ -5,3 +5,8 @@ const wordDef = require('word-definition');
 const sqlite3 = require('sqlite3');
 
 let app = express();
+
+
+wordDef.getDef("Help", "en", null, function(def) {
+    console.log(def);
+});
