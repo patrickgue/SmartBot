bca602e6edca831f8c430b0e4064438b9b265f39
patrickgue <me@patrickguenthard.ch>
2016-10-28 15:05:12 +0200

    added word count database and added test application to 'feed it with words'

--- bot.js
+++ bot.js
@@ -1,18 +1,92 @@
 "use strict";
 
 const express = require('express');
-const wd = require('word-definition');
 const sqlite3 = require('sqlite3');
 const https = require("https");
 const http = require("http");
+const db = require('./services/dbservice.js');
+const bodyParser = require('body-parser');
+
 
 let app = express();
 
 
-https.get("https://en.wikipedia.org/wiki/", function(data) {
-    console.log(data);
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(function(req, res, next) {
+    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
-wd.getDef("keyboard", "en", null, function(definition) {
-    console.log(definition);
+/*
+for(let word of words) {
+    db.run("INSERT INTO TSBT_WORD (wordName) VALUES(\"" + word + "\") ");
+}*/
+
+app.post("/message/", function(req, res) {
+    let message = req.body.message.replace(/[,-]./g,"");
+    let sentenceArray = message.split(/[.?!:;]./g);
+    let sentences = [];
+
+    for(let j = 0; j < sentenceArray.length; j++) {
+	let sentence = sentenceArray[j];
+	let wordArray = sentence.split(" ");
+	let words = [];
+
+	for(let i = 0; i < wordArray.length; i++) {
+	    let word = wordArray[i]
+	    db.select("SELECT * FROM TSBT_WORD WHERE wordName = \""+word.toLowerCase()+"\"", function(data) {
+		console.log(data);
+		if(data.length > 0) {
+		    db.run("UPDATE TSBT_WORD SET wordUsage = ? WHERE wordId = ?", [(data[0].wordUsage + 1), data[0].wordId]);
+		    words.push(data);
+		}
+		else {
+		    db.run("INSERT INTO TSBT_WORD (wordName) VALUES (?)",[word.toLowerCase()], function(data) {
+			words.push({
+			    wordId : data,
+			    wordName : word,
+			    wordUsage : 0
+			});
+		    });
+		}
+	    });
+
+	    if(i+1 === wordArray.length) {
+		sentenceDone(words);
+	    }
+	}
+	console.log(j);
+	if(j+1 === sentenceArray.length) {
+	    messageDone();
+	}
+    }
+
+    function messageDone() {
+	res.end(JSON.stringify(sentences));
+    }
+
+    function sentenceDone(words) {
+	sentences.push(words);
+    }
+    
+    
 });
+
+
+
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(host, port);
+});
+
+
+
+
+
---
+++ services/dbservice.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var file = "./data/database.db";
+var exists = fs.existsSync(file);
+var db = new sqlite3.Database(file);
+
+db.serialize(function(){
+    if(!exists) {
+
+	/* tables */
+	db.run("CREATE TABLE TSBT_USER ("
+               + "userId INTEGER PRIMARY KEY AUTOINCREMENT, "
+               + "userName STRING, "
+               + "userPassword STRING"
+               + ")");
+	
+	db.run("CREATE TABLE TSBT_WORD ("
+	       + "wordId INTEGER PRIMARY KEY AUTOINCREMENT, "
+	       + "wordName STRING, "
+	       + "wordUsage INTEGER DEFAULT 0"
+	       + ")");
+	db.run("CREATE TABLE TSBT_SENTENCE ("
+	       + "sentenceId INTEGER PRIMARY KEY AUTOINCREMENT, "
+	       + "sentenceUsage INTEGER DEFAULT 0"
+	       + ")");
+
+	db.run("CREATE TABLE TSBT_WORD_SENTENCE ("
+	       + "wordSentenceId INTEGER PRIMARY KEY AUTOINCREMENT, "
+	       + "wordSentenceWordId INTEGER, "
+	       + "wordSentencePosition INTEGER"
+	       + ")")
+	
+	
+    }
+});
+
+
+function select(query, done) {
+    var rows = [];
+    db.each(query, function(err,row) {
+	rows.push(row);
+    }, function() {
+	if(done !== undefined) {
+	    done(rows);
+	}
+    });
+}
+
+function run(query, argsarray, done) {
+    var stmt = db.prepare(query);
+    stmt.run(argsarray);
+    stmt.finalize();
+    select("SELECT last_insert_rowid();", function(d) {
+	if(done !== undefined){
+	    done(d[0]["last_insert_rowid()"]);
+	}
+    });
+
+}
+
+module.exports = {
+    select : select,
+    run : run
+}
