7c06599d4bc7047d1342c79a1448920d5b319a29
patrickgue <me@patrickguenthard.ch>
2016-10-30 00:25:09 +0200

    corrected port

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ app.post("/message/", function(req, res) {
 
 
 
-var server = app.listen(8080, function() {
+var server = app.listen(4084, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log(host, port);
