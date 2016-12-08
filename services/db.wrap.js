115efc00b0fc484cda82c4c1fba7980e8ee41a17
patrickgue <me@patrickguenthard.ch>
2016-12-08 23:29:54 +0100

    Minor Modifications to original V8 bot

--- services/db.wrap.js
+++ services/db.wrap.js
@@ -1,8 +1,11 @@
 "use strict";
 
-const db = require("./dbservice.mysql.js");
+const db = require("./dbservice.js");
 
 
+function aQ(str) { // Add Quotes
+    return "\"" + str + "\"";
+}
 
 
 function dbWrap(table) {
@@ -10,7 +13,12 @@ function dbWrap(table) {
     let sqlString = "";
 
     function doneRun(fun) {
+	console.log(sqlString);
 	db.run(sqlString,[],fun);
+    }
+
+    function doneSel(fun) {
+	console.log(sqlString);
     }
     
     return {
@@ -20,7 +28,7 @@ function dbWrap(table) {
 	    
 	    return {
 		"values" : function(values) {
-		    sqlString += " VALUES(\""+values.join("\",\"")+"\")"
+		    sqlString += " VALUES(\""+values.join(aQ(","))+"\")";
 		    return {
 			"done" : doneRun
 		    };
@@ -28,14 +36,61 @@ function dbWrap(table) {
 	    }
 	},
 	"update" : function(valuesObj) {
-	    sqlString += "UPDATE " + table + " ";
-	    return {
-		"where" : function() {
+	    sqlString += "UPDATE " + table + " SET ";
 
+	    let setComma = false;
+	    for(let index in valuesObj) {
+		if (setComma) {
+		    sqlString += ", ";
+		}
+		else {
+		    setComma = true;
+		}
+
+		let valueTerm = valuesObj[index];
+		
+		if (typeof valuesObj[index] === "string") {
+		    valueTerm = aQ(valuesObj[index]);
+		}
+		
+		sqlString += index + " = " + valueTerm;
+	    }
+	    
+	    return {
+		"where" : function(sqlStr) {
+		    sqlString += " WHERE " + sqlStr;
+		    return {
+			done : doneRun
+		    }
 		},
 		"done" : doneRun
 	    }
-	} 
+	},
+	"select" : function(fields) {
+	    sqlString = "SELECT ";
+	    sqlString += fields.join(", ") + " FROM " + table + " ";
+	    return {
+		where : function(whereStr) {
+		    sqlString += "WHERE " + whereStr + " ";
+		    return {
+			done : doneSel,
+			group : function(groupStr) {
+			    sqlString += "GROUP BY " + groupStr + " ";
+			    return {
+				done : doneSel,
+				order : function(orderStr) {
+				    sqlString += "ORDER BY " + orderStr + " ";
+				    return {
+					done : doneSel
+				    }
+				}
+			    }
+			}
+		    }
+		},
+		done : doneSel
+	    };
+	}
 
     };
     
--- services/db.wrap.test.js
+++ services/db.wrap.test.js
@@ -3,4 +3,6 @@
 const db = require("./db.wrap.js");
 
 
-db("TSBT_WORD").insert(["wordName"]).values(["désoleé"]).done();
+//db("TSBT_WORD").insert(["wordName"]).values(["hello"]).done();
+
+db("TSBT_WORD").update({wordUsage : 1}).where("wordName = \"hello\"").done();
